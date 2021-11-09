9189e31a7cae474667e0fb0ef4fe2b73b7ad8d92
Edgar Uco <edgarin@gmail.com>
2021-11-09 13:24:54 -0600

    target images added

--- script.js
+++ script.js
@@ -110,6 +110,8 @@ const player = new Player();
 
 //Bubbles
 const bubbles = [];
+
+
 class Bubble {
       constructor() {
             this.x = Math.random() * canvas.width;
@@ -128,12 +130,15 @@ class Bubble {
       }
       draw() {
             //Se dibujan los circulos objetivo.
-            ctx.fillStyle = "blue";
+            /* ctx.fillStyle = "blue";
             ctx.beginPath();
             ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
             ctx.fill();
             ctx.closePath();
-            ctx.stroke();
+            ctx.stroke(); */
+            this.image = new Image();
+            this.image.src = '/images/bubble_pop.png';
+            ctx.drawImage(this.image, this.x - 50, this.y - 50, this.radius * 2, this.radius * 2);
       }
 }
 
